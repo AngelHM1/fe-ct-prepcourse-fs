17d1f671e5fe784f210d2f2093369419996e47aa
Angelhdz0 <angelhernandezslayermail.com>
2023-10-10 00:18:30 -0600

    avances

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -2,49 +2,67 @@
 /*3️⃣ EJERCICIO 03 3️⃣*/
 
 function sonIguales(x, y) {
-   // Retorna true si "x" e "y" son iguales.
-   // De lo contrario, retorna false.
-   // Tu código:
-   return x===y
+  // Retorna true si "x" e "y" son iguales.
+  // De lo contrario, retorna false.
+  // Tu código:
+  return x === y;
 }
-var resultado = sonIguales(10,10)
-console.log(resultado)
+//var resultado = sonIguales(9, 9);
+//console.log(resultado);
+
 function tienenMismaLongitud(str1, str2) {
-   // Retorna true si los dos strings tienen la misma longitud.
-   // De lo contrario, retorna false.
-   // Tu código:
+  // Retorna true si los dos strings tienen la misma longitud.
+  // De lo contrario, retorna false.
+  // Tu código:
+  return str1.length === str2.length;
 }
+
+//var resultado = tienenMismaLongitud("buenas", "bueno");
+//console.log(resultado);
 
 function menosQueNoventa(num) {
-   // Retorna true si el argumento "num" es menor que noventa.
-   // De lo contrario, retorna false.
-   // Tu código:
+  // Retorna true si el argumento "num" es menor que noventa.
+  // De lo contrario, retorna false.
+  // Tu código:
+  return num < 90;
 }
+//var resultado = menosQueNoventa(89);
+//console.log(resultado);
 
 function mayorQueCincuenta(num) {
-   // Retorna true si el argumento "num" es mayor que cincuenta.
-   // De lo contrario, retorna false.
-   // Tu código:
+  // Retorna true si el argumento "num" es mayor que cincuenta.
+  // De lo contrario, retorna false.
+  // Tu código:
+  return num > 50;
 }
+//var resultado = mayorQueCincuenta(55);
+//console.log(resultado);
 
 function esPar(num) {
-   // Retorna true si "num" es par.
-   // De lo contrario, retorna false.
-   // Tu código:
+  // Retorna true si "num" es par.
+  // De lo contrario, retorna false.
+  // Tu código:
+  var residuo = num % 2;
+  if (residuo === 0) return true;
+  else return false;
 }
+//var resultado = esPar(2);
+//console.log(resultado);
 
 function esImpar(num) {
-   // Retorna true si "num" es impar.
-   // De lo contrario, retorna false.
-   // Tu código:
+  // Retorna true si "num" es impar.
+  // De lo contrario, retorna false.
+  // Tu código:
+  var resultado = esPar(num);
+  if (resultado === false) return true;
+  else return false;
 }
-
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   tienenMismaLongitud,
-   sonIguales,
-   menosQueNoventa,
-   mayorQueCincuenta,
-   esPar,
-   esImpar,
+  tienenMismaLongitud,
+  sonIguales,
+  menosQueNoventa,
+  mayorQueCincuenta,
+  esPar,
+  esImpar,
 };
--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -2,70 +2,110 @@
 /*5️⃣ EJERCICIO 05 5️⃣*/
 
 function esPositivo(num) {
-   // La función recibe un entero. Devuelve como resultado un string que indica si el número
-   // es positivo o negativo.
-   // Si el número es positivo ---> "Es positivo".
-   // Si el número es negativo ---> "Es negativo".
-   // Si el número es 0, devuelve false.
-   // Tu código:
+  // La función recibe un entero. Devuelve como resultado un string que indica si el número
+  // es positivo o negativo.
+  // Si el número es positivo ---> "Es positivo".
+  // Si el número es negativo ---> "Es negativo".
+  // Si el número es 0, devuelve false.
+  // Tu código:
+  if (num > 0) return "Es positivo";
+  else if (num < 0) return "Es negativo";
+  return false;
 }
+//var resultado = esPositivo(-56);
+//console.log(resultado);
 
 function agregarSimboloExclamacion(str) {
-   // Agrega un símbolo de exclamación al final del string "str" y retórnalo
-   // Ejemplo: "hello world" ---> "hello world!"
-   // Tu código:
+  // Agrega un símbolo de exclamación al final del string "str" y retórnalo
+  // Ejemplo: "hello world" ---> "hello world!"
+  // Tu código:
+  return str + "!";
 }
+
+//var resultado = agregarSimboloExclamacion("Hasta luego");
+//console.log(resultado);
 
 function combinarNombres(nombre, apellido) {
-   // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
-   // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
-   // Tu código:
+  // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
+  // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
+  // Tu código:
+  return nombre + " " + apellido;
 }
+
+//var resultado = combinarNombres("angel", "hernandez");
+//console.log(resultado);
 
 function obtenerSaludo(nombre) {
-   // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
-   // Ejemplo: "Martin" ---> "Hola Martin!"
-   // Tu código:
+  // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
+  // Ejemplo: "Martin" ---> "Hola Martin!"
+  // Tu código:
+  return "Hola" + " " + nombre + "!";
 }
+//var resultado = obtenerSaludo("Angel");
+//onsole.log(resultado);
 
 function obtenerAreaRectangulo(alto, ancho) {
-   // Retornar el área de un rectángulo teniendo su altura y ancho.
-   // Tu código:
+  // Retornar el área de un rectángulo teniendo su altura y ancho.
+  // Tu código:
+  return alto * ancho;
 }
+//var resultado = obtenerAreaRectangulo(10, 20);
+//console.log(resultado);
 
 function retornarPerimetro(lado) {
-   // La función recibe como argumento la medida de un lado de un cuadrado.
-   // Debes retornar su perímetro.
-   // Tu código:
+  // La función recibe como argumento la medida de un lado de un cuadrado.
+  // Debes retornar su perímetro.
+  // Tu código:
+  return 4 * lado;
 }
+
+//var resultado = retornarPerimetro(5);
+//console.log(resultado);
 
 function areaDelTriangulo(base, altura) {
-   // Calcula el área de un triángulo y retorna el resultado.
-   // Tu código:
+  // Calcula el área de un triángulo y retorna el resultado.
+  // Tu código:
+  return (base * altura) / 2;
 }
 
+//var resultado = areaDelTriangulo(50, 100);
+//console.log(resultado);
+
 function deEuroAdolar(euro) {
-   // Supongamos que 1 euro equivale a 1.20 dólares.
-   // Debes calcular el valor recibido como argumento pasándolo a dolares.
-   // Tu código:
+  // Supongamos que 1 euro equivale a 1.20 dólares.
+  // Debes calcular el valor recibido como argumento pasándolo a dolares.
+  // Tu código:
+  return euro * 1.2;
 }
 
 function esVocal(letra) {
-   // Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
-   // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
-   // Si no es vocal, tambien debe retornar "Dato incorrecto".
-   // Tu código:
+  // Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
+  // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
+  // Si no es vocal, tambien debe retornar "Dato incorrecto".
+  // Tu código:
+  letra = letra.toUpperCase();
+  if (
+    letra == "A" ||
+    letra == "E" ||
+    letra == "I" ||
+    letra == "O" ||
+    letra == "U"
+  ) {
+    return "Es vocal";
+  } else {
+    return "Dato incorrecto";
+  }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   esPositivo,
-   agregarSimboloExclamacion,
-   combinarNombres,
-   obtenerSaludo,
-   obtenerAreaRectangulo,
-   retornarPerimetro,
-   areaDelTriangulo,
-   deEuroAdolar,
-   esVocal,
+  esPositivo,
+  agregarSimboloExclamacion,
+  combinarNombres,
+  obtenerSaludo,
+  obtenerAreaRectangulo,
+  retornarPerimetro,
+  areaDelTriangulo,
+  deEuroAdolar,
+  esVocal,
 };
